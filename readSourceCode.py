37956cb138b78bfd2395faa9fcfa6757708e6874
beltonhe <belton.he@hotmail.com>
2021-09-02 19:07:40 -0600

    lab1 code

--- readSourceCode.py
+++ readSourceCode.py
@@ -1,4 +1,5 @@
 import requests
+import inspect
 r1 = requests.get("https://raw.githubusercontent.com/beltonhe/cmput404lab1/master/Version.py")
 r2 = requests.get("https://raw.githubusercontent.com/beltonhe/cmput404lab1/master/curl.py")
 print(r1.text)
