8dbd7416e269784572706e0ab83c37c571292337
beltonhe <belton.he@hotmail.com>
2021-09-03 10:50:48 -0600

    lab1 code

--- read.py
+++ read.py
@@ -1,3 +1,17 @@
 import requests
+
+## 1. version of request
+## version check code from Damien Francois 
+## https://stackoverflow.com/users/1763614/damienfrancois
+## on https://stackoverflow.com/questions/20180543/how-to-check-version-of-python-modules
+print(requests.__version__)
+
+## 2. curl
+## // use code from https://www.kite.com/python/answers/how-to-use-curl-in-python to complete python GET
+url = "http://google.com"
+res = requests.get(url)
+print(res.text)
+
+
 r = requests.get("https://raw.githubusercontent.com/beltonhe/cmput404lab1/master/AllInOne.py")
 print(r.text)
