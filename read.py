6a8d87d1b5a681fcfe549d3f71ac1ee1abf498a6
beltonhe <belton.he@hotmail.com>
2021-09-03 10:48:15 -0600

    lab1 code

--- read.py
+++ read.py
@@ -1,5 +1,3 @@
 import requests
-r1 = requests.get("https://raw.githubusercontent.com/beltonhe/cmput404lab1/master/Version.py")
-r2 = requests.get("https://raw.githubusercontent.com/beltonhe/cmput404lab1/master/curl.py")
-print(r1.text)
-print(r2.text)
+r = requests.get("https://raw.githubusercontent.com/beltonhe/cmput404lab1/master/AllInOne.py")
+print(r.text)
